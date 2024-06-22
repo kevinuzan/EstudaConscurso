1b3b29240ab05aa22e2d459d3785af2b0ef60272
kevinuzan <kevinuzan@hotmail.com>
2024-06-22 17:24:44 -0300

    ALTERAÇÃO DE SERVER PARA USE

--- server.mjs
+++ server.mjs
@@ -24,15 +24,27 @@ app.use('/img', express.static(path.join(__dirname, 'public/img')));
 app.use('/temp_folder', express.static(path.join(__dirname, 'public/temp_folder')));
 app.use('/', express.static(path.join(__dirname, '/index.html')));
 
+// CORS HEADERS MIDDLEWARE
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token, x-refresh-token, _id");
+
+  res.header(
+      'Access-Control-Expose-Headers',
+      'x-access-token, x-refresh-token'
+  );
+
+  next();
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.get('/changeJsonData', function (req, res) {
-  var newDataAppend = req.query.name.split(';')[0]
-  var id = req.query.name.split(';')[1]
-  var resultado = changeJson(newDataAppend, id)
-  res.json(resultado)
+app.post('/lists', function (req, res) {
+  console.log('TESTADO')
+  res.send({ success: true })
 });
 
 /**
@@ -85,7 +97,12 @@ async function mongoDbQuery() {
   }
 }
 
-mongoDbQuery()
+//mongoDbQuery()
+
+
+
+
+
 // var connectionString = process.env.DATABASE_URL
 // var pgClient = new pg.Client(connectionString)
 // pgClient.connect()
